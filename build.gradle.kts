ad7f517d3d49bf033218e8fc3c85709509f3e991
Jacquigee <gitaujaquiline@gmail.com>
2024-11-23 13:48:37 +0300

    Fixes double response bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import org.gradle.internal.impldep.org.eclipse.jgit.util.RawCharUtil.trimTrailingWhitespace
-import org.jetbrains.kotlin.builtins.StandardNames.FqNames.target
-import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
